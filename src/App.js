177a7dbfe630bc6295a8d90a131e12216de85521
Ayse Batur Ozturk <aysebaturozturk@192.168.1.93>
2024-02-10 09:45:05 +0300

    added cart visibility state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShow] = useState(false);
+
+  function showCartHandler(){
+      setCartIsShow(true);
+  }
+
+  function hideCartHandler(){
+      setCartIsShow(false);
+  }
+
   return (
     <CartProvider>
       <Header />
